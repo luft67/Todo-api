ceaff0eab200f1f340e33c1f70317abe36b8df67
David Petterson <david.petterson@weightwatchers.com>
2016-05-17 16:21:56 -0400

    Add PUT todos

--- server.js
+++ server.js
@@ -66,6 +66,35 @@ app.delete('/todos/:id', function (req, res){
   }
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matched = _.findWhere(todos, {id: todoId});
+  var body = _.pick(req.body, 'description', 'completed');
+  var validAttributes = {};
+
+  if (!matched) {
+    return res.status(400).send();  // use return to stop program from running past this point
+  }
+
+  if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')) {
+    return res.status(400).send();
+  }
+
+  if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+    validAttributes.description = body.description;
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
+  }
+
+  //_.extend(destination, sources)
+  _.extend(matched, validAttributes);
+  res.json(matched);
+
+
+});
 
 app.listen(PORT, function () {
   console.log('Express listening on ' + PORT + '...');
